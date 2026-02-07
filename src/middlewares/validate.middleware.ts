1c05cc13e642ccf65e7ac5fe43bfaed8aee5f1f4
Sumudu Maduranga <madu.sumudu1999@gmail.com>
2026-02-07 09:24:33 +0530

    Update package.json to include @types/nodemailer and refactor validateBody to use ZodTypeAny

--- src/middlewares/validate.middleware.ts
+++ src/middlewares/validate.middleware.ts
@@ -1,9 +1,9 @@
-import { AnyZodObject } from "zod";
+import { ZodTypeAny } from "zod";
 import { NextFunction, Request, Response } from "express";
 import { ApiError } from "./error.middleware";
 
 export const validateBody =
-  (schema: AnyZodObject) =>
+  (schema: ZodTypeAny) =>
   (req: Request, _res: Response, next: NextFunction) => {
     const result = schema.safeParse(req.body);
     if (!result.success) {
